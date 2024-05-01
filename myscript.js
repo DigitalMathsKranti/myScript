7b0570ca28767fed16503c4e51d4fd0960577cda
Ram Kumar <digitalmathskranti@gmail.com>
2024-05-01 14:19:14 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,19 @@
+/* photo Running One By One */
+let image =document.querySelector('.slider_image');
+let translate=0;
+setInterval(()=>{
+    if (translate>=0 && translate<1000) /* (PhotoCount -1) *100 */
+    {
+        translate+=100;
+    }
+    else
+    {
+        translate=0;
+    }
+   
+    console.log (`${-translate}%`);
+    image.style.transition='2s';
+    image.style.transform = `translateX(${-translate}%)`;
+
+},5000)
+/* photo Running One By One */
